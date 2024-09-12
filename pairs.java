52b865132cc4d04ab2e4395f429dc7eb9ba052fc
Varun Joshi <121236149+varunjoshi27@users.noreply.github.com>
2024-09-13 00:04:44 +0530

    pairs in array

---
+++ pairs.java
@@ -0,0 +1,19 @@
+public class pairs {
+    public static void pairs(int numbers[]){
+        int ps=0;
+        for(int i=0;i<numbers.length;i++){
+            int curr=numbers[i];
+            for(int j=i+1;j<numbers.length;j++){
+                System.out.print("("+curr+","+numbers[j]+")");
+                ps++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs:"+ps);
+    }
+public static void main(String[] args) {
+    int numbers[]={1,2,3,4,5};
+    pairs(numbers);
+}
+    
+}
