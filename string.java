62394088f9530d48e5a8c894b8846e3c2671d398
Varun Joshi <121236149+varunjoshi27@users.noreply.github.com>
2024-09-14 01:15:27 +0530

    Palindrome number

---
+++ string.java
@@ -0,0 +1,26 @@
+/**
+ * string
+ */
+public class string {
+public static boolean ispallindrome(String number){
+    for(int i=0;i<number.length()/2;i++){
+        int n=number.length();
+        if (number.charAt(i)!=number.charAt(n-i-1)) {
+            return false;
+            
+        }
+    }
+    return true;
+}
+    public static void main(String[] args) {
+        String number="12345654321";
+        // System.out.println("The number is"+ispallindrome(number));
+        boolean name=ispallindrome(number);
+        if (name!=true) {
+            System.out.println("The number is not palindrome");
+            
+        }else{
+            System.out.println("TTHe number is a palindrome number");
+        }
+    }
+}
